dd76a2412f8647c60b0156960149ee7d8b107cf2
Fathah KA <48355244+fathah@users.noreply.github.com>
2026-06-26 16:58:57 +0530

    Discord Join

---
+++ app/social/discord/contributor/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function ContributorPage() {
+  redirect('https://api.hermesone.org/api/discord/verify/contributor')
+}
---
+++ app/social/discord/hdclub/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function HdClubPage() {
+  redirect('https://api.hermesone.org/api/discord/verify/token')
+}
